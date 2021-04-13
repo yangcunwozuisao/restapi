312c897889483ae7145310dde0d10be1d54e4b32
Tomaz <tmsski@yahoo.com.br>
2021-04-13 17:59:35 -0300

    Versão inicial

---
+++ src/main/java/ps2/restapi/_App.java
@@ -0,0 +1,13 @@
+package ps2.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(_App.class, args);
+	}
+
+}
